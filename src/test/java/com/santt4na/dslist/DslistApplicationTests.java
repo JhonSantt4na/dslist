78aac2d57c3f998f70737ba1aa6b221449d16300
Santt4naDev <jhonnsantt4na@gmail.com>
2025-01-21 22:48:46 -0300

    Project Created

---
+++ src/test/java/com/santt4na/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4na.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
